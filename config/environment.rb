ea9c47765bc268dda8184d53c278ad4d041e1c06
Anton Davydov <antondavydov.o@gmail.com>
2016-04-24 09:25:15 +0300

    Generate empty admin app

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/rodauth_hanami'
+require_relative '../apps/admin/application'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
+  mount Admin::Application, at: '/admin'
   mount Web::Application, at: '/'
 end
